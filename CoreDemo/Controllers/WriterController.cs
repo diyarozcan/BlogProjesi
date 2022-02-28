05bdba8482009ab6404a994936a189b9d6b68566
diyarozcan <ozcan.diyar@hotmail.com>
2022-02-28 18:05:27 +0300

    Add project files.

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartialView()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writervalues = wm.GetById(1);
+            return View(writervalues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+
+            if (results.IsValid)
+            {
+                wm.TUpdate(p);
+                return RedirectToAction("Index","Dashboard");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id=1,
+                    Username="Diyar"
+                },
+                new UserComment
+                {
+                    Id=2,
+                    Username="Ferhat"
+                },
+                new UserComment
+                {
+                    Id=3,
+                    Username="Özgür"
+                },
+            };
+            return View(commentvalues);
+        }
+    }
+}
